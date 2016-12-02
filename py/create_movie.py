f29c6f6539ab24363ba2ab20228e7ba94c925ed9
Thierry Fredrich <thierry@lusi.uni-sb.de>
2016-12-02 17:24:47 +0059

    removed double line

--- py/create_movie.py
+++ py/create_movie.py
@@ -19,7 +19,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
-# -*- coding: utf-8 -*-
+
 #http://zulko.github.io/blog/2013/09/27/read-and-write-video-frames-in-python-using-ffmpeg/
 
 ''' usage:
